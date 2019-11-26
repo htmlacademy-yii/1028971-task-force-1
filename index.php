7a4c02983da12486b6955c710618372c41c74c63
Yulia <youlka25@yandex.ru>
2019-11-26 19:39:14 +0300

    класс Convert2

--- index.php
+++ index.php
@@ -3,9 +3,20 @@ require_once __DIR__ . '\vendor\autoload.php';
 
 use src\import\Converter2;
 
-echo "<pre>";
-$file_map = [];
-print_r(Converter2::getSqlFromCsv('data\users.csv', $file_map, 'user'));
+$value_map = [
+    'email' => 0,
+    'name' => 1,
+    'password' => 2,
+    'reg_date' => 3,
+    'city_id' => function(){return rand(1, 1008);},
+    'is_executor' => function(){return rand(0,1);},
+    'some_key' => function(){return rand(1, 100);}
+];
+
+var_dump(Converter2::getSqlFromCsv('data\users.csv', $value_map, 'user'));
+
+
+
 
 
 
--- src/import/Converter2.php
+++ src/import/Converter2.php
@@ -12,20 +12,17 @@ class Converter2
         $file = new \SplFileObject($file_name);
         $file->setFlags(8);
         $headers = $file->current();
-        $sql = '';
+        $sql = [];
         foreach ($file as $row) {
+            $lineMap = [];
             if ($row != $headers && !in_array(null, $row)) {
-                $value_map['email'] = $row[0];
-                $value_map['name'] = $row[1];
-                $value_map['password'] = $row[2];
-                $value_map['reg_date'] = $row[3];
-                $value_map['city_id'] = rand(1, 1008);
-                $value_map['is_executor'] = rand(0, 1);
-
-                $values = "'" . implode("','", array_values($value_map)) . "'";
-                $columns = implode(',', array_keys($value_map));
-
-                $sql .= "INSERT INTO $table_name ($columns) VALUE($values)" . "\n";
+                foreach ($value_map as $fieldName => $fieldValue) {
+                    $lineMap[$fieldName] = is_int($fieldValue) ? $row[$fieldValue] : $fieldValue();
+                }
+                $sql[] = "INSERT INTO $table_name (" . implode(', ', array_keys($lineMap)) . ")
+                 VALUES (" . implode(', ', array_map(function (&$value) {
+                        return "'" . $value . "'";
+                    }, $lineMap)) . ")";
             }
         }
 
