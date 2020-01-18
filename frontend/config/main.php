76876345e86d9f0b2fab86ab299b62110a16390b
Yulia <youlka25@yandex.ru>
2020-01-18 09:35:49 +0300

    в frontend/config/main.php добавлен дефолтный раут - tasks/index

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -9,6 +9,7 @@ $params = array_merge(
 return [
     'id' => 'app-frontend',
     'language' => 'RU-ru',
+    'defaultRoute' => 'tasks/index',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'controllerNamespace' => 'frontend\controllers',
